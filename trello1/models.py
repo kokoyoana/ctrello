d6118ba83faa2af5c6c8501bbac1e3a4e11b16e3
kokoyoana <koko-yoana@hotmail.es>
2020-02-11 16:52:51 +0100

    bd y modelos
    Creamos base de datos y modelos

--- trello1/models.py
+++ trello1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tablero(models.Model):
+    nombre = models.CharField(max_length=50)
+
+class Lista(models.Model):
+    nombreLista = models.CharField(max_length=50)
+    fkTablero = models.ForeignKey(Tablero,on_delete=models.CASCADE)
+class Tarea(models.Model):
+    nombreTare = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=200)
+    fkLista = models.ForeignKey(Lista,on_delete=models.CASCADE)
+
