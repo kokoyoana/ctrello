b72c0a4bb5f06fa6f5513926ebad7d8a810607bf
kokoyoana <koko-yoana@hotmail.es>
2020-02-12 15:53:53 +0100

    crud creado

--- trello1/admin.py
+++ trello1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class TableroAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+
+class ListaAdmin(admin.ModelAdmin):
+    list_display = ('nombreLista','fkTablero')
+
+
+
+class TareaAdmin(admin.ModelAdmin):
+    list_display = ('nombreTare','descripcion','fkLista')
+
+
+admin.site.register(Tablero,TableroAdmin)
+admin.site.register(Lista,ListaAdmin)
+admin.site.register(Tarea,TareaAdmin)
